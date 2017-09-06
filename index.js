cc74548db3d02de19df8285bdca3093e52ad2fbf
Seth Barden <sethbarden@gmail.com>
2017-09-06 21:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function takeANumber(array, name) {
 }
 
 function nowServing(array) {
+  name = array.shift()
   if (array.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${array[0]}.`
-    array.shift()
+    return `Currently serving ${name}.`
   }
 }
 
