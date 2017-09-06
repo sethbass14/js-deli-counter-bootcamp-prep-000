003d23effdb121221274840964c4e94eab1b3c41
Seth Barden <sethbarden@gmail.com>
2017-09-06 21:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function nowServing(array) {
     array.shift()
   }
 }
+
+function currentLine(array) {
+  var string = "The line is currently "
+  if (array.length === 0) {
+    string = string + "empty."
+  } else {
+      for (i = 0; i < array.length; i++) {
+        string = string + `${i + 1}. ${array[i]},  `
+      }
+  }
+  return string
+}
