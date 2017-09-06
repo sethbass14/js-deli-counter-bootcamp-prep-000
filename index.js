b5f6e0816dc52854de1479c208b195e98b53b170
Seth Barden <sethbarden@gmail.com>
2017-09-06 21:29:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function currentLine(array) {
   } else {
       for (var i = 0; i < array.length; i++) {
         string = string + `${i + 1}. ${array[i]}, `
+        string = string.slice(-1)
       }
   }
   return string
